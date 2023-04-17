25ca49d3f065ae2abaa5b88d27143896c27fff75
Moritz Roessler <moritz.roessler@gmail.com>
2023-04-17 17:18:31 +0200

    feat: add comments component

--- src/server-components/examples/Comments.tsx
+++ src/server-components/examples/Comments.tsx
@@ -6,6 +6,7 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   Chip,
   IconButton,
   List,
@@ -13,18 +14,24 @@ import {
   ListItemSecondaryAction,
   ListItemText,
   TextField,
+  Tooltip,
   Typography,
 } from '@mui/material';
 import { authContext, useComponent } from '@state-less/react-client';
 import HeartIcon from '@mui/icons-material/Favorite';
 import { useContext, useState } from 'react';
 import GoogleIcon from '@mui/icons-material/Google';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const Comments = ({ id = 'comments' }) => {
   const { session, authenticate } = useContext(authContext);
   const [component, { error, loading }] = useComponent(id, {});
   const [comment, setComment] = useState('');
   const comments = component?.props?.comments || [];
+
+  const canComment = session?.id;
+  const canDelete = component?.props?.permissions.delete;
+
   return (
     <Card>
       {loading && <Alert severity="info">Loading...</Alert>}
@@ -33,8 +40,14 @@ export const Comments = ({ id = 'comments' }) => {
         <Alert severity="info">You need to be logged in to comment.</Alert>
       )}
 
-      {comments.map((comment) => {
-        return <Comment comment={comment} />;
+      {comments.map((comment, index) => {
+        return (
+          <Comment
+            comment={comment}
+            del={() => component?.props?.del(index)}
+            canDelete={canDelete}
+          />
+        );
       })}
 
       <CardContent>
@@ -43,10 +56,25 @@ export const Comments = ({ id = 'comments' }) => {
           rows={3}
           onChange={(e) => setComment(e.target.value)}
           fullWidth
+          value={comment}
         />
       </CardContent>
       <CardActions>
-        <Button onClick={() => component?.props?.comment(comment)}>Add</Button>
+        <Tooltip
+          title={canComment ? '' : 'You need to be logged in to comment.'}
+        >
+          <span>
+            <Button
+              onClick={() => {
+                component?.props?.comment(comment);
+                setComment('');
+              }}
+              disabled={!canComment}
+            >
+              Add
+            </Button>
+          </span>
+        </Tooltip>
       </CardActions>
     </Card>
   );
@@ -55,12 +83,19 @@ export const Comments = ({ id = 'comments' }) => {
 const StrategyIcons = {
   google: GoogleIcon,
 };
-const Comment = ({ comment }) => {
+const Comment = ({ comment, del, canDelete }) => {
   const Icon = StrategyIcons[comment.strategy];
   return (
     <Card sx={{ m: 1 }}>
       <CardContent sx={{ display: 'flex' }}>
         <Typography variant="body1">{comment.message}</Typography>
+      </CardContent>
+      <CardActions>
+        {canDelete && (
+          <IconButton onClick={del}>
+            <DeleteIcon />
+          </IconButton>
+        )}
         <Chip
           avatar={
             <Avatar src={comment.identity.picture}>
@@ -70,7 +105,7 @@ const Comment = ({ comment }) => {
           label={comment.identity.name}
           sx={{ ml: 'auto' }}
         ></Chip>
-      </CardContent>
+      </CardActions>
     </Card>
   );
 };
