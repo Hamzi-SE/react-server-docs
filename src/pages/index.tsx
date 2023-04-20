c61939fa275bc363301e16a66db5fdc9e22264f4
Moritz Roessler <morrossl@publicisgroupe.net>
2023-04-20 10:31:58 +0200

    fix: add ml:1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export const IndexPage = () => {
   return (
     <Grid container spacing={1}>
       <Grid item xs={12} md={4} xl={3} sx={{ mt: 1 }}>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} sx={{ ml: 1 }}>
           <Grid item xs={12}>
             <Poll id="poll-open" />
           </Grid>
