20927a31ccfc031440f7f6fd17e1ae0c18913a46
Moritz Roessler <morrossl@publicisgroupe.net>
2023-04-20 10:24:05 +0200

    fix: don't remove padding

--- src/server-components/examples/Polls.tsx
+++ src/server-components/examples/Polls.tsx
@@ -22,7 +22,7 @@ export const Poll = ({ id = 'poll' }) => {
         {component?.props?.values?.map((value, i) => {
           const percentage = (100 / sum) * component?.props?.votes[i];
           return (
-            <ListItem dense disablePadding>
+            <ListItem dense>
               <Box
                 sx={{
                   ml: -2,
