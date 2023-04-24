27d0193990d091b8c464b97869bb633deef2f85b
Moritz Roessler <moritz.roessler@gmail.com>
2023-04-24 23:12:42 +0200

    fix: remove left debug message

--- src/server-components/examples/Polls.tsx
+++ src/server-components/examples/Polls.tsx
@@ -16,7 +16,6 @@ export const Poll = ({ id = 'poll', message }) => {
   const sum = component?.props?.votes.reduce((a, b) => a + b, 0);
   return (
     <Card>
-      {id}
       {loading && <Alert severity="info">Loading...</Alert>}
       {error && <Alert severity="error">{error.message}</Alert>}
       {message && message?.(component?.props || {})}
